3f27cee8f4a3e6a39cb7f17d79ee93ad10843255
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-10-21 17:30:48 +0200

    correct container names and Dockerfile

--- docker/docker-run-ec3users-shared.sh
+++ docker/docker-run-ec3users-shared.sh
@@ -9,16 +9,16 @@ fi
 
 docker rm ec3-cloud-users; \
 docker run --privileged --rm -ti \
--h isabella \
+-h ec3-cloud-users \
 -e ZDOTDIR="/mnt" \
 --log-driver json-file --log-opt max-size=10m \
 -v /dev/log:/dev/log \
 -v /etc/localtime:/etc/localtime:ro \
 -v $HOME:/mnt \
--v $HOME/my_work/srce/git.ec3-cloud-users/ec3-cloud-users/docker/pysitepkg:/root/pysitepkg \
--v $HOME/my_work/srce/git.ec3-cloud-users/ec3-cloud-users/docker/syncsite.sh:/root/syncsite.sh \
--v $HOME/my_work/srce/git.ec3-cloud-users/ec3-cloud-users/:/root/ec3-cloud-users \
--v $HOME/my_work/srce/git.ec3-cloud-users/ec3-cloud-users/config_values/:/etc/ec3-cloud-users-frontend:ro \
+-v $HOME/my_work/srce/git.ec3-cloud-users/ec3-cloud-users/docker/pysitepkg:/home/user/pysitepkg \
+-v $HOME/my_work/srce/git.ec3-cloud-users/ec3-cloud-users/docker/syncsite.sh:/home/user/syncsite.sh \
+-v $HOME/my_work/srce/git.ec3-cloud-users/ec3-cloud-users/:/home/user/ec3-cloud-users \
+-v $HOME/my_work/srce/git.ec3-cloud-users/ec3-cloud-users/config_values/:/etc/ec3-cloud-users:ro \
 -v $HOME/my_work/srce/git.ec3-cloud-users/ec3-cloud-users/modules/:/usr/lib/python2.7/site-packages/ec3_cloud_users:ro \
 -v $HOME/my_work/srce/git.ec3-cloud-users/ec3-cloud-users/bin/create-accounts.py:/usr/libexec/ec3-cloud-users/create-accounts.py:ro \
 -v $HOME/my_work/srce/git.ec3-cloud-users/ec3-cloud-users/bin/setup-db.py:/usr/libexec/ec3-cloud-users/setup-db.py:ro \
--- docker/docker-run-ec3users.sh
+++ docker/docker-run-ec3users.sh
@@ -2,12 +2,12 @@
 
 docker rm ec3-cloud-users; \
 docker run --privileged --rm -ti \
--h isabella \
+-h ec3-cloud-users \
 -e ZDOTDIR="/mnt" \
 --log-driver json-file --log-opt max-size=10m \
 -v /dev/log:/dev/log \
 -v /etc/localtime:/etc/localtime:ro \
 -v $HOME:/mnt \
--v /home/daniel/my_work/srce/git.ec3-cloud-users/ec3-cloud-users:/root/ec3-cloud-users \
+-v /home/daniel/my_work/srce/git.ec3-cloud-users/ec3-cloud-users:/home/user/ec3-cloud-users \
 --name ec3-cloud-users \
 ipanema:5000/ec3-cloud-users /bin/zsh
