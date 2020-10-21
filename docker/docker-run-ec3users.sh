3aecc251b38cc6c31f0e270cabfd9ccffc2af3e5
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-10-21 16:52:58 +0200

    correct docker mount paths

--- docker/docker-run-ec3users.sh
+++ docker/docker-run-ec3users.sh
@@ -8,6 +8,6 @@ docker run --privileged --rm -ti \
 -v /dev/log:/dev/log \
 -v /etc/localtime:/etc/localtime:ro \
 -v $HOME:/mnt \
--v /home/daniel/my_work/srce/git.isabella-users/isabella-users/:/root/isabella-users \
+-v /home/daniel/my_work/srce/git.ec3-cloud-users/ec3-cloud-users:/root/ec3-cloud-users \
 --name ec3-cloud-users \
 ipanema:5000/ec3-cloud-users /bin/zsh
