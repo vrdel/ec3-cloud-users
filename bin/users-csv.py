e283b4d1bc56a989c767e979156950bddd8c9f16
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-09-19 14:50:50 +0200

    load users from csv and cache and make diff

--- bin/users-csv.py
+++ bin/users-csv.py
@@ -6,6 +6,12 @@ import os
 import csv
 
 from ec3_cloud_users.log import Logger
+from ec3_cloud_users.cache import load, update
+from ec3_cloud_users.userutils import UserUtils
+from ec3_cloud_users.config import parse_config
+from ec3_cloud_users.userutils import gen_username
+
+conf_opts = parse_config()
 
 
 def main():
@@ -16,7 +22,10 @@ def main():
     lobj = Logger(sys.argv[0])
     logger = lobj.get()
 
-    users = list()
+    usertool = UserUtils(logger)
+    users, usernames = list(), set()
+    cdb = conf_opts['settings']['cache']
+    cache = load(cdb, logger)
 
     with open(args.csvfile) as fp:
         reader = csv.reader(fp.readlines(), delimiter=',')
@@ -24,8 +33,16 @@ def main():
             users.append(row)
 
     users = users[1:]
-    print(users[0])
+    allusernames_db = set([u['username'] for u in cache['users']])
+
+    for user in users:
+        usernames.update([gen_username(user[0], user[1], allusernames_db)])
+
+    diff = usernames.difference(allusernames_db)
+
+    for users in diff:
+        print(users)
+
 
 if __name__ == '__main__':
     main()
-
